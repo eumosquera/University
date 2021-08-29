4ca8d46f7014d5c3618adce209aa86edd8abf395
Ender Mosquera Urrutia <75103273+eumosquera@users.noreply.github.com>
2021-08-29 13:18:06 -0500

    Crear models

---
+++ University.BL/DTOs/InstructorDTO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace University.BL.DTOs
+{
+    public class InstructorDTO
+    {
+
+        [Display(Name = "ID")]
+        [Required(ErrorMessage = "El campo ID es requerido")] //ID OPCIONAL PORQUE NO SE ENVIA EN LA VISTA
+        public int ID { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "El campo Last Name es requerido")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        public string LastName { get; set; }
+
+        [Display(Name = "First MidName")]
+        [Required(ErrorMessage = "El campo First MidName es requerido")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        public string FirstMidName { get; set; }
+
+        [Display(Name ="Hire Date")]
+        [Required(ErrorMessage ="El campo Hire Date es requerido")]
+        [DataType(DataType.DateTime)]
+        public DateTime HireDate { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}", LastName, FirstMidName);
+            }
+        }
+    }
+}
---
+++ University.BL/Models/Instructor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.BL.Models
+{
+    [Table("Instructor", Schema ="dbo")]
+    public class Instructor
+    {
+        [Key]
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime HireDate { get; set; }
+
+    }
+}
