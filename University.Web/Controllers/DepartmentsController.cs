39751ba54c1f9f67d573db16c754675d3bb3078b
Ender Mosquera Urrutia <75103273+eumosquera@users.noreply.github.com>
2021-09-05 01:50:36 -0500

    DEPARTMENTS CONTROLLER AND INDEX VIEW

--- University.Web/Controllers/DepartmentsController.cs
+++ University.Web/Controllers/DepartmentsController.cs
@@ -1,17 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using University.BL.Models;
+using University.BL.DTOs;
+using University.BL.Data;
+using System.Linq;
+using PagedList;
+
 
 namespace University.Web.Controllers
 {
     public class DepartmentsController : Controller
     {
+        private readonly UniversityContext context = new UniversityContext();
+
         // GET: Departments
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(int? departmentid, int? pageSize, int? page)
         {
-            return View();
+            #region Consluta Deparments
+            var query = context.Departments.ToList();
+
+            var deparments = query.Where(x => x.StartDate < DateTime.Now)
+                .Select(x => new DepartmentDTO
+                {
+                    DepartmentID = x.DepartmentID,
+                    Name = x.Name,
+                    Budget = x.Budget,
+                    StartDate = x.StartDate
+
+                }).ToList();
+            #endregion
+            #region Paginacion
+            //Si viene nulo dele 10 por defecto
+            pageSize = (pageSize ?? 10);
+            //si viene igual por defecto llevelo a la 1 
+            page = (page ?? 1);
+            ViewBag.pageSize = pageSize;
+            #endregion 
+            return View(deparments.ToPagedList(page.Value, pageSize.Value));
+
         }
-    }
-}
+
+    }//FIN PUBLIC CLASS CONTROLLER
+
+} // FIN NAMESPACE
